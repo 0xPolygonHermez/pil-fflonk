760477a10054d089f706992c73a6185408d40d53
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2023-07-04 07:47:13 +0000

    refactor step42ns_first  prototype function

--- src/chelpers/pilfflonk.chelpers.step42ns.cpp
+++ src/chelpers/pilfflonk.chelpers.step42ns.cpp
@@ -1,7 +1,7 @@
 #include <alt_bn128.hpp>
 #include "pilfflonk_steps.hpp"
 
-AltBn128::FrElement PilFflonkSteps::step42ns_first(AltBn128::Engine &E, StepsParams &params, uint64_t i) {
+void PilFflonkSteps::step42ns_first(AltBn128::Engine &E, StepsParams &params, uint64_t i) {
      AltBn128::FrElement tmp_0 = E.fr.mul(params.cm1_2ns[0 + i*3], params.cm1_2ns[1 + i*3]);
      AltBn128::FrElement tmp_8 = E.fr.sub(tmp_0, params.cm1_2ns[2 + i*3]);
      AltBn128::FrElement tmp_1 = E.fr.sub(params.cm1_2ns[2 + i*3], params.publicInputs[0]);
@@ -14,8 +14,9 @@ AltBn128::FrElement PilFflonkSteps::step42ns_first(AltBn128::Engine &E, StepsPar
      AltBn128::FrElement tmp_6 = E.fr.add(tmp_5, tmp_9);
      AltBn128::FrElement tmp_7 = E.fr.mul(params.challenges[4], tmp_6);
      AltBn128::FrElement tmp_11 = E.fr.add(tmp_7, tmp_10);
-     return tmp_11;
+     E.fr.copy((AltBn128::FrElement &)(params.q_2ns[i]), tmp_11);
 }
+
 void PilFflonkSteps::step42ns_i(AltBn128::Engine &E, StepsParams &params, uint64_t i) {
      AltBn128::FrElement tmp_0 = E.fr.mul(params.cm1_2ns[0 + i*3], params.cm1_2ns[1 + i*3]);
      AltBn128::FrElement tmp_8 = E.fr.sub(tmp_0, params.cm1_2ns[2 + i*3]);
