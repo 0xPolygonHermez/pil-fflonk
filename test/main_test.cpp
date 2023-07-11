260445b162c32359656982ab58ecb9a09661d17c
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2023-07-11 14:05:29 +0000

    refactor tests

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,8 +1,5 @@
 #include <gtest/gtest.h>
-
-#include "ntt_altbn128.test.hpp"
-#include "polinomial.test.hpp" 
-#include "polynomial.test.hpp"
+#include <alt_bn128.hpp>
 
 // // Test fixture for common setup and teardown
 // class MyTestFixture : public ::testing::Test {
@@ -21,39 +18,5 @@ AltBn128::Engine E;
 int main(int argc, char** argv) { 
     ::testing::InitGoogleTest(&argc, argv);
     
-    // Check the command-line arguments to execute desired test(s) 
-    bool bn128 = false;
-    bool polinomial = false;
-    bool polynomial = false;
-
-    for (int i = 1; i < argc; ++i) {
-        if (std::string(argv[i]) == "--bn128") {  
-            bn128 = true;
-        } else if (std::string(argv[i]) == "--polinomial") {  
-            polinomial = true;
-        }
-    }
-
-    if (!bn128 && !polinomial) {
-        return RUN_ALL_TESTS();
-    }
-  
-    int result = 0;
- 
-    if (bn128) {
-        result |= RUN_ALL_TESTS(); // Run all tests and aggregate the results
-        result &= ~1; // Clear the lowest bit to ignore test2's result
-    }
-
-    if (polinomial) {
-        result |= RUN_ALL_TESTS();
-        result &= ~2; // Clear the second lowest bit to ignore test1's result
-    }
-
-    if (polynomial) {
-        result |= RUN_ALL_TESTS();
-        result &= ~3; // Clear the second lowest bit to ignore test1's result
-    }
-
-    return result;
+    return RUN_ALL_TESTS();
 }
--- test/ntt_altbn128.test.cpp
+++ test/ntt_altbn128.test.cpp
@@ -361,23 +361,3 @@ TEST(BN128_TEST, extendePol)
     free(a);
     free(b);
 }
-
-// int main(int argc, char **argv)
-// {
-//     ::testing::InitGoogleTest(&argc, argv);
-//     return RUN_ALL_TESTS();
-// }
-
-// Build commands AVX:
-
-// g++:
-//  g++ tests/tests.cpp src/* -lgtest -lgmp -lomp -o test -g  -Wall -pthread -fopenmp -mavx2 -L$(find /usr/lib/llvm-* -name "libomp.so" | sed 's/libomp.so//')
-//  Intel:
-//  icpx tests/tests.cpp src/*.cpp -o test -lgtest -lgmp  -pthread -fopenmp -mavx2
-
-// Build commands AVX512:
-
-// g++:
-//  g++ tests/tests.cpp src/* -lgtest -lgmp -lomp -o test -g  -Wall -pthread -fopenmp -mavx2  -mavx512f -L$(find /usr/lib/llvm-* -name "libomp.so" | sed 's/libomp.so//') -D__AVX512__
-//  Intel:
-//  icpx tests/tests.cpp src/*.cpp -o test -lgtest -lgmp  -pthread -fopenmp -mavx2 -mavx512f -D__AVX512__
